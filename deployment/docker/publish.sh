49dc951b89032676c2e53fb02c2dc737e88642ae
Steve Carrupt <steve@codefactoring.com>
2019-08-17 12:25:55 +1000

    Log in Docker Hub so the images can be pushed

--- deployment/docker/publish.sh
+++ deployment/docker/publish.sh
@@ -5,4 +5,6 @@ set -e
 # Trick to get directory that script is located in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker-compose -f ${DIR}/docker-compose-build.prod.yaml push
