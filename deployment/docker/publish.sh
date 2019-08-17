dcc484d627d144ac3ecceaaf1fef68f4ecdd6b59
Steve Carrupt <steve@codefactoring.com>
2019-08-17 11:45:01 +1000

    Fix scripts paths so they can run from travis

--- deployment/docker/publish.sh
+++ deployment/docker/publish.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-docker-compose -f docker-compose-build.prod.yaml push
+# Trick to get directory that script is located in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+docker-compose -f ${DIR}/docker-compose-build.prod.yaml push
--- deployment/k8s/deploy.sh
+++ deployment/k8s/deploy.sh
@@ -2,20 +2,22 @@
 
 set -e
 
-configmap=`cat "./env-configmap.yaml" | sed "s/{{AWS_BUCKET}}/$AWS_BUCKET/g;s/{{AWS_PROFILE}}/$AWS_PROFILE/g;s/{{AWS_REGION}}/$AWS_REGION/g;s/{{POSTGRESS_DB}}/$POSTGRESS_DB/g;s/{{POSTGRESS_HOST}}/$POSTGRESS_HOST/g;s#{{APP_URL}}#$APP_URL#g"`
+# Trick to get directory that script is located in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+configmap=`cat "$DIR/env-configmap.yaml" | sed "s/{{AWS_BUCKET}}/$AWS_BUCKET/g;s/{{AWS_PROFILE}}/$AWS_PROFILE/g;s/{{AWS_REGION}}/$AWS_REGION/g;s/{{POSTGRESS_DB}}/$POSTGRESS_DB/g;s/{{POSTGRESS_HOST}}/$POSTGRESS_HOST/g;s#{{APP_URL}}#$APP_URL#g"`
 echo "$configmap" | kubectl apply -f -
 
-secret=`cat "./env-secret.yaml" | sed "s/{{JWT_SECRET}}/$JWT_SECRET/g;s/{{POSTGRESS_USERNAME}}/$POSTGRESS_USERNAME/g;s/{{POSTGRESS_PASSWORD}}/$POSTGRESS_PASSWORD/g"`
+secret=`cat "$DIR/env-secret.yaml" | sed "s/{{JWT_SECRET}}/$JWT_SECRET/g;s/{{POSTGRESS_USERNAME}}/$POSTGRESS_USERNAME/g;s/{{POSTGRESS_PASSWORD}}/$POSTGRESS_PASSWORD/g"`
 echo "$secret" | kubectl apply -f -
 
-awsCredentials=`cat ~/.aws/credentials | base64`
-awsSecret=`cat "./aws-secret.yaml" | sed "s/{{AWS_CREDENTIALS}}/$awsCredentials/g"`
+awsSecret=`cat "$DIR/aws-secret.yaml" | sed "s/{{AWS_CREDENTIALS}}/$AWS_CREDENTIALS/g"`
 echo "$awsSecret" | kubectl apply -f -
 
-kubectl apply -f ./backend-feed-deployment.yaml
+kubectl apply -f $DIR/backend-feed-deployment.yaml
 
-kubectl apply -f ./backend-user-deployment.yaml
+kubectl apply -f $DIR/backend-user-deployment.yaml
 
-kubectl apply -f ./reverseproxy-deployment.yaml
+kubectl apply -f $DIR/reverseproxy-deployment.yaml
 
-kubectl apply -f ./frontend-deployment.yaml
+kubectl apply -f $DIR/frontend-deployment.yaml
